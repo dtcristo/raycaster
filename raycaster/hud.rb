761378f7069fbb789aab7883d5010d7aca5f707a
David Cristofaro <david@dtcristo.com>
2017-03-10 23:26:23 +1100

    Render FPS on HUD

--- raycaster/hud.rb
+++ raycaster/hud.rb
@@ -11,7 +11,7 @@ module Raycaster
       y = @player.y.round(2)
       angle = (@player.direction * 180 / Math::PI).round
       @font.draw(
-        "x: #{x}, y: #{y}, direction: #{angle}°", 10, 10, 1, 1.0, 1.0, @color
+        "fps: #{Gosu.fps}, x: #{x}, y: #{y}, direction: #{angle}°", 10, 10, 1, 1.0, 1.0, @color
       )
     end
   end
