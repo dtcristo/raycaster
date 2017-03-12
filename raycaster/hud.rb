041022ecf67df80fb0b3827180acdd88a1c93ed4
David Cristofaro <david@dtcristo.com>
2017-03-12 17:07:42 +1100

    Refactor HUD using translate

--- raycaster/hud.rb
+++ raycaster/hud.rb
@@ -22,34 +22,36 @@ module Raycaster
     end
 
     def draw_map
-      (0..@map.size-1).each do |x|
-        (0..@map.size-1).each do |y|
-          next unless @map.get(x, y) > 0
-          left = @map_origin[:x]+x*@map_scale
-          top = @map_origin[:y]+y*@map_scale
-          Gosu.draw_rect(
-            @map_origin[:x] + x*@map_scale,
-            @map_origin[:y] + y*@map_scale,
-            @map_scale, @map_scale, Gosu::Color::WHITE
-          )
+      Gosu.translate(@map_origin[:x], @map_origin[:y]) do
+        # Draw cells
+        (0..@map.size-1).each do |x|
+          (0..@map.size-1).each do |y|
+            next unless @map.get(x, y) > 0
+            Gosu.draw_rect(
+              x*@map_scale, y*@map_scale,
+              @map_scale, @map_scale, Gosu::Color::WHITE
+            )
+          end
         end
+        # Draw player
+        Gosu.draw_rect(
+          @player.x*@map_scale - @half_player_size,
+          @player.y*@map_scale - @half_player_size,
+          @player_size, @player_size, Gosu::Color::RED
+        )
       end
-
-      Gosu.draw_rect(
-        @map_origin[:x] + @player.x*@map_scale - @half_player_size,
-        @map_origin[:y] + @player.y*@map_scale - @half_player_size,
-        @player_size, @player_size, Gosu::Color::RED
-      )
     end
 
     def draw_stats
-      x = @player.x.round(1)
-      y = @player.y.round(1)
-      angle = Gosu.radians_to_degrees(@player.direction).round
-      @font.draw("fps: #{Gosu.fps}", 20, 20, 1, 1, 1, @color)
-      @font.draw("x: #{x}", 20, 20 + @font_size, 1, 1, 1, @color)
-      @font.draw("y: #{y}", 20, 20 + 2*@font_size, 1, 1, 1, @color)
-      @font.draw("direction: #{angle}°", 20, 20 + 3*@font_size, 1, 1, 1, @color)
+      Gosu.translate(20, 20) do
+        x = @player.x.round(1)
+        y = @player.y.round(1)
+        angle = Gosu.radians_to_degrees(@player.direction).round
+        @font.draw("fps: #{Gosu.fps}", 0, 0, 1, 1, 1, @color)
+        @font.draw("x: #{x}", 0, @font_size, 1, 1, 1, @color)
+        @font.draw("y: #{y}", 0, 2*@font_size, 1, 1, 1, @color)
+        @font.draw("direction: #{angle}°", 0, 3*@font_size, 1, 1, 1, @color)
+      end
     end
   end
 end
